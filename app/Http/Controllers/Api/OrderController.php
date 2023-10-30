6fe224278ec6516cf0c249b6c6beb67d2d414428
MostafaElebzary <mostafaelebzary@gmail.com>
2023-10-30 19:51:50 +0200

    payment method

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\User\OrderRequest;
+use App\Http\Requests\Api\User\StoreTransactionIdRequest;
 use App\Http\Resources\Api\User\AddressesResources;
 use App\Http\Resources\Api\User\OrderDetailsResource;
 use App\Http\Resources\Api\User\OrderResource;
@@ -14,6 +15,7 @@ use App\Models\OrderItem;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class OrderController extends Controller
 {
@@ -24,10 +26,10 @@ class OrderController extends Controller
         $box = Box::whereId($request->box_id)->first();
 //        todo::validate that total price less than max
 
-        if ($request->main_category_id !=null){
-            $products_sum = Product::whereIn('id',$request->products_id)
+        if ($request->main_category_id != null) {
+            $products_sum = Product::whereIn('id', $request->products_id)
                 ->sum('sel_price');
-            if ($products_sum > $box->max_price){
+            if ($products_sum > $box->max_price) {
                 return msg(false, trans('lang.invalid_products'), failed());
             }
         }
@@ -95,4 +97,71 @@ class OrderController extends Controller
         $data = new OrderDetailsResource($order);
         return msgdata(true, trans('lang.data_display_success'), $data, success());
     }
+
+
+    public function storeOrderTransactionId(StoreTransactionIdRequest $request)
+    {
+        $order = Order::whereId($request->order_id)
+            ->update([
+                'transaction_id' => $request->transaction_id,
+                'payment_method' => $request->payment_method,
+            ]);
+
+        return msg(true, trans('lang.Success_text'), success());
+    }
+
+    public function payOrder(Request $request)
+    {
+        $payment = $this->checkPayment($request->tap_id);
+        if (isset($payment->status) && $payment->status == 'CAPTURED') {
+            try {
+                DB::beginTransaction();
+                // find and update order by transaction_id
+                $order = Order::where('transaction_id', $request->tap_id)
+                    ->update([
+                        "payment_status" => "paid"
+                    ]);
+                DB::commit();
+            } catch (\Exception $e) {
+                DB::rollback();
+                throw $e;
+            }
+            return msg(true, trans('lang.Success_text'), success());
+        } else {
+            return msg(false, trans('lang.Message_Fail'), failed());
+        }
+    }
+
+    public function checkPayment($charge_id)
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, [
+            CURLOPT_URL => "https://api.tap.company/v2/charges/" . $charge_id,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => [
+                "Authorization: Bearer sk_test_pJ8K61wBTgO3WzXRaf5omI7D",
+                "accept: application/json",
+                "content-type: application/json"
+            ],
+        ]);
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+//        if ($err) {
+//            echo "cURL Error #:" . $err;
+//        } else {
+//        }
+        return json_decode($response);
+    }
+
+
 }
