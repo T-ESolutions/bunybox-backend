f6475294e18b3eaf01f62b2d4adc2352f1425181
MostafaElebzary <mostafaelebzary@gmail.com>
2023-12-03 13:28:31 +0200

    test

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -61,6 +61,7 @@ class HomeController extends Controller
             ->orderBy('id', 'asc')->get();
 
 
+        dd($boxes);
 
         foreach ($boxes as $key => $box) {
             $product_array = [];
@@ -83,7 +84,6 @@ class HomeController extends Controller
 
         }
 
-        dd($boxes);
 
 
         $result['boxes'] = BoxFinalResource::customCollection($boxes, $data);
